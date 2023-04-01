9671f1ce26eee1967cd39b99d7f0c2a0372c98fb
talat2311 <talatmcc@outlook.com>
2023-04-01 19:26:53 +0600

    second commit

--- setup.c
+++ setup.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
-int main(){
-    printf("hello world tomal");
+int main() {
+    char name[50];
+    int age;
+
+    printf("Enter your name: ");
+    scanf("%s", name);
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    printf("Hello, %s! You are %d years old.\n", name, age);
 
     return 0;
 }
+
